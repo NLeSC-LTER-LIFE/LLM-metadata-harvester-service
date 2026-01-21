bdb901aaf707b31172e4bb8e4243b51c86b1d8aa
Meiert Grootes <mwgrootes@Meierts-MacBook-Pro.local>
2026-01-21 14:50:20 +0100

    adapted celery addresses to be environment driven

--- src/llm_metadata_harvester_service/core/celery_app.py
+++ src/llm_metadata_harvester_service/core/celery_app.py
@@ -1,9 +1,14 @@
 from celery import Celery
+import os
+
+broker_url = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+result_backend = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
+
 
 celery_app = Celery(
     "worker",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/1",
+    broker=broker_url,
+    backend=result_backend,
 )
 
 celery_app.conf.update(
